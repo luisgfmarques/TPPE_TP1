590072272608f99e6cbb92f11d983e705a663a54
Luis Marques <luisgustavomarques@outlook.com>
2022-12-18 20:40:02 -0300

    refatoração dos testes

--- functions.py
+++ functions.py
@@ -1,14 +1,17 @@
+from math import inf
+
+
 def return_ultima_faixas(valor):
-    valores_limite = [1903.98, 2826.65, 3751.05, 4664.68]
-    faixas = [1, 2, 3, 4, 5]
-    aliquotas = [0, 7.5, 15, 22.5, 27.5]
+    valores_limite = [
+        {"valor": 1903.98, "aliquota": 0},
+        {"valor": 2826.65, "aliquota": 7.5 / 100},
+        {"valor": 3751.05, "aliquota": 15 / 100},
+        {"valor": 4664.68, "aliquota": 22.5 / 100},
+        {"valor": inf, "aliquota": 27.5 / 100},
+    ]
     for i in range(len(valores_limite)):
-        if valor <= valores_limite[i]:
+        if valor <= valores_limite[i]["valor"]:
             return {
-                "faixa": faixas[i],
-                "aliquota": aliquotas[i],
+                "faixa": i + 1,
+                "aliquota": valores_limite[i]["aliquota"],
             }
-    return {
-        "faixa": faixas[-1],
-        "aliquota": aliquotas[-1],
-    }
--- tests/tests_app.py
+++ tests/tests_app.py
@@ -3,7 +3,7 @@ from ..functions import return_ultima_faixas
 
 
 def test_return_ultima_faixa():
-    assert return_ultima_faixas(2000)["aliquota"] == 7.5
+    assert return_ultima_faixas(2000)["aliquota"] == 0.075
     assert return_ultima_faixas(2000)["faixa"] == 2
-    assert return_ultima_faixas(50000)["aliquota"] == 27.5
+    assert return_ultima_faixas(50000)["aliquota"] == 0.275
     assert return_ultima_faixas(50000)["faixa"] == 5
