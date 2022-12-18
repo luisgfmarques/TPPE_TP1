23ec490293aa014bbf122e39a6bde93ce96fec57
Luis Marques <luisgustavomarques@outlook.com>
2022-12-18 20:32:51 -0300

    adição de tests para confirmação que a função esta realmente funcionando

--- tests/tests_app.py
+++ tests/tests_app.py
@@ -1,7 +1,9 @@
 import pytest
-from ..functions import return_faixas
+from ..functions import return_ultima_faixas
 
 
-def test_return_faixas():
-    assert return_faixas(2000)["aliquota"] == 7.5
-    assert return_faixas(2000)["faixa"] == 2
+def test_return_ultima_faixa():
+    assert return_ultima_faixas(2000)["aliquota"] == 7.5
+    assert return_ultima_faixas(2000)["faixa"] == 2
+    assert return_ultima_faixas(50000)["aliquota"] == 27.5
+    assert return_ultima_faixas(50000)["faixa"] == 5
