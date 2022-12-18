1baefeb6b466d4118e2b398305d4e4f6dad4b832
Luis Marques <luisgustavomarques@outlook.com>
2022-12-18 20:32:26 -0300

    implementação da função para tirar a falsificação

--- functions.py
+++ functions.py
@@ -1,5 +1,14 @@
-def return_faixas(valor):
+def return_ultima_faixas(valor):
+    valores_limite = [1903.98, 2826.65, 3751.05, 4664.68]
+    faixas = [1, 2, 3, 4, 5]
+    aliquotas = [0, 7.5, 15, 22.5, 27.5]
+    for i in range(len(valores_limite)):
+        if valor <= valores_limite[i]:
+            return {
+                "faixa": faixas[i],
+                "aliquota": aliquotas[i],
+            }
     return {
-        "faixa": 2,
-        "aliquota": 7.5,
+        "faixa": faixas[-1],
+        "aliquota": aliquotas[-1],
     }
