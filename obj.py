73806d0e60e24cb124ef4a3bebb686519c5fa823
Luis Marques <luisgustavomarques@outlook.com>
2022-12-18 21:31:36 -0300

    criação do objeto pessoa, juntamente com função de cadastro de rendimento

---
+++ obj.py
@@ -0,0 +1,19 @@
+class PessoaFisicaReceitaFederal:
+
+    name = None
+    cpf = None
+    rendimentos = []
+    soma_rendimentos_tributaveis = 0
+    deducao = None
+
+    def cadastrar_rendimentos(self, valor: float, descricao: str):
+        if valor < 0 or valor is None:
+            raise Exception("ValorRendimentoInvalidoException")
+        if descricao is None or descricao == "":
+            raise Exception("DescricaoEmBrancoException")
+        self.rendimentos.append({"valor": valor, "descricao": descricao})
+        self.soma_rendimentos_tributaveis += valor
+
+
+def printtest():
+    return "test"
