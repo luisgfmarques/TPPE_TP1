47d0938746ff05a61a7cb69ed9772fa6c145722c
Luis Marques <luisgustavomarques@outlook.com>
2023-01-29 08:39:00 -0300

    Refactoring: extrair metodo para metodo objeto Mau(s) cheiro(s) identificado(s): método calcula_valor_imposto ainda muito longo. Descrição da refatoração: tranformados os metodos calcula_valor_impiosto_faixa e atualiza_valor_calculo como metodos de um novo objeto denominado CalculaValores Classes/métodos/atributos afetados: CalculaValores, calcula_valor_imposto_faixa e atualiza_valor_calculo, valores_limite

--- functions.py
+++ functions.py
@@ -39,13 +39,26 @@ def append_value(faixa: int, valores_limite: list, valor_calculo: float):
     }
 
 
-def calcula_valor_imposto_faixa(valores_limite: list, faixa: int):
-    return valores_limite[faixa]["valor"] * valores_limite[faixa]["aliquota"]
+class CalculaValores:
+    valores_limite = []
+    base_de_calculo = 0
 
+    def __init__(self, base_de_calculo: float):
+        self.base_de_calculo = base_de_calculo
+        self.valores_limite = [
+            {"valor": 1903.98, "aliquota": 0},
+            {"valor": 2826.65, "aliquota": 7.5 / 100},
+            {"valor": 3751.05, "aliquota": 15 / 100},
+            {"valor": 4664.68, "aliquota": 22.5 / 100},
+            {"valor": base_de_calculo - 4664.68, "aliquota": 27.5 / 100},
+        ]
 
-def atualiza_valor_calculo(valor_calculo: float, valores_limite: list, faixa: int):
-    valor_calculo -= valores_limite[faixa]["valor"]
-    return round(valor_calculo, 3)
+    def calcula_valor_imposto_faixa(self, valores_limite: list, faixa: int):
+        return valores_limite[faixa]["valor"] * valores_limite[faixa]["aliquota"]
+
+    def atualiza_valor_calculo(self, valor_calculo: float, faixa: int):
+        valor_calculo -= self.valores_limite[faixa]["valor"]
+        return round(valor_calculo, 3)
 
 
 def adiciona_faixa_total(base_calculo: float, valor_imposto: float):
@@ -54,27 +67,26 @@ def adiciona_faixa_total(base_calculo: float, valor_imposto: float):
         "faixa de base de calculo": base_calculo,
         "aliquota da faixa": "-",
         " imposto total": round(valor_imposto, 4),
-    }   
+    }
+
 
 def calcula_valor_imposto(base_calculo: float):
-    valores_limite = [
-        {"valor": 1903.98, "aliquota": 0},
-        {"valor": 922.67, "aliquota": 7.5 / 100},
-        {"valor": 924.40, "aliquota": 15 / 100},
-        {"valor": 913.63, "aliquota": 22.5 / 100},
-        {"valor": base_calculo - 4664.68, "aliquota": 27.5 / 100},
-    ]
+    valores = CalculaValores(base_calculo)
     valor_calculo = base_calculo
     valor_imposto = 0
     faixa = 0
     valores = []
     while valor_calculo > 0:
-        valores.append(append_value(faixa, valores_limite, valor_calculo))
-        if valor_calculo >= valores_limite[faixa]["valor"]:
-            valor_imposto += calcula_valor_imposto_faixa(valores_limite, faixa)
-            valor_calculo = atualiza_valor_calculo(valor_calculo, valores_limite, faixa)
+        valores.append(append_value(faixa, valores.valores_limite, valor_calculo))
+        if valor_calculo >= valores.valores_limite[faixa]["valor"]:
+            valor_imposto += CalculaValores.calcula_valor_imposto_faixa(
+                valores.valores_limite, faixa
+            )
+            valor_calculo = CalculaValores.atualiza_valor_calculo(
+                valor_calculo, valores.valores_limite, faixa
+            )
         else:
-            valor_imposto += valor_calculo * valores_limite[faixa]["aliquota"]
+            valor_imposto += valor_calculo * valores.valores_limite[faixa]["aliquota"]
             valor_calculo = 0
         faixa += 1
     valores.append(adiciona_faixa_total(base_calculo, valor_imposto))
